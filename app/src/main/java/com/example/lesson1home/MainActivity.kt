00a35924eadaf2418ccd2889722b4312d2e123a4
ThePhyrus <romaisgood@yandex.ru>
2022-03-28 18:33:52 +0300

    Наскоряк переписанное дз.

--- app/src/main/java/com/example/lesson1home/MainActivity.kt
+++ app/src/main/java/com/example/lesson1home/MainActivity.kt
@@ -9,47 +9,43 @@ private const val TAG:String = "@@@"
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var person: Person
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
 
         val data = Data(1, 1, 1)
         val data2 = data.copy()
         data2.age = 100
 
-        Log.d("@@@", "Data  age: $data")
-        Log.d("@@@", "Data2 age: $data2")
+        Log.d(TAG, "Data  age: $data")
+        Log.d(TAG, "Data2 age: $data2")
 
-        for (one in getArrayList()) {
-            Log.d("@@@", "for $one")
+        for (one in arrList()) {
+            Log.d(TAG, "for $one")
         }
-        getArrayList().forEach {
-            Log.d("@@@", "forEach $it")
+        arrList().forEach {
+            Log.d(TAG, "forEach $it")
         }
 
-        for (i in 1..10){
-            Log.d("@@@", "for in 1..10 $i")
-        }
         for (i in 1 until 10){
-            Log.d("@@@", "for until $i")
+            Log.d(TAG, "cycle for until $i")
+        }
+        for (i in 1..10){
+            Log.d(TAG, "cycle for in range 1..10 $i")
         }
         for (i in 10 downTo  1 step 3){
-            Log.d("@@@", "for downTo $i")
+            Log.d(TAG, "for downTo $i")
         }
-        var counter = 10
-        while (counter-->0){
-            Log.d("@@@", "while counter $counter")
+        var counter1 = 10
+        while (counter1-->0){
+            Log.d(TAG, "while counter $counter1")
         }
-        counter = 10
+        var counter2 = 10
         do {
-            Log.d("@@@", "do while counter $counter")
-        }while (counter-->0)
+            Log.d(TAG, "do while counter $counter2")
+        }while (counter2-->0)
 
     }
 
-    private fun getArrayList() = listOf(1, 2, 3, 4, 5, 8)
+    private fun arrList() = listOf(1, 2, 3, 4, 5, 8)
 }
